374218a0e3c68e1f80460da47bbf1463d44c4e46
wuchunfu <319355703@qq.com>
2021-01-23 13:48:54 +0800

    Solving cross domain problems

--- middleware/cors/cors.go
+++ middleware/cors/cors.go
@@ -1,10 +1,8 @@
 package cors
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"strings"
 )
 
 func Cors() gin.HandlerFunc {
@@ -13,27 +11,16 @@ func Cors() gin.HandlerFunc {
 		method := ctx.Request.Method
 		// 请求头部
 		origin := ctx.Request.Header.Get("Origin")
-		// 声明请求头keys
-		var headerKeys []string
-		for k, _ := range ctx.Request.Header {
-			headerKeys = append(headerKeys, k)
-		}
-		headerStr := strings.Join(headerKeys, ", ")
-		if headerStr != "" {
-			headerStr = fmt.Sprintf("Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Request-Headers, Access-Control-Request-Method, %s", headerStr)
-		} else {
-			headerStr = "Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Request-Headers, Access-Control-Request-Method"
-		}
 		if origin != "" {
 			// 这是允许访问所有域
 			ctx.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 			// 服务器支持的所有跨域请求的方法,为了避免浏览次请求的多次'预检'请求
-			ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+			ctx.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 			// header的类型
-			ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Access-Control-Request-Headers, Access-Control-Request-Method, User-Agent, Authorization, Content-Length, X-CSRF-Token, X-Token, Token, session, X_Requested_With, Accept, Accept-Encoding, Origin Referer, Host, Connection, Accept-Language, DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma, Sec-Fetch-Mode, Sec-Fetch-Site, Sec-Fetch-Dest")
+			ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Token, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, X-CSRF-Token, X-Token, Token")
 			// 允许跨域设置, 可以返回其他子段
 			// 跨域关键设置 让浏览器可以解析
-			ctx.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type, Expires, Last-Modified, Pragma, FooBar")
+			ctx.Writer.Header().Set("Access-Control-Expose-Headers", "*")
 			// 缓存请求信息 单位为秒
 			ctx.Writer.Header().Set("Access-Control-Max-Age", "172800")
 			//  跨域请求是否需要带cookie信息 默认设置为true
